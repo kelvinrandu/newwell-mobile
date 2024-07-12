60f60340fe9b0026ccc471ed31bcf02f506647b6
kelvinrandu <randukelvin@gmail.com>
2024-07-12 11:19:27 +0300

    added login style

--- StickerSmash/src/screens/Login.jsx
+++ StickerSmash/src/screens/Login.jsx
@@ -7,8 +7,10 @@ import {
   useNavigationContainerRef,
 } from '@react-navigation/native';
 import { useNavigation } from '@react-navigation/native';
-import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
-
+import {
+  // Button,
+   StyleSheet, Text, TextInput, View} from 'react-native';
+   import { Button } from 'react-native-paper';
 
 // const URL_KEY = 'API2axhtoc6oUxm';
 // const TOKEN_KEY = 'SVVLMwejKKlqmgf9LV255fWOmEeLLEYM41O1Ma1rIZ6B';
@@ -23,36 +25,49 @@ export const Login = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={{color: colors.text}}>LOGIN</Text>
+       <Text textAlign='left' style={{color: colors.text,fontSize:20,fontWeight:800, paddingBottom:180}}>Newwell for Artists</Text>
+      <Text textAlign='left' style={{color: colors.text}}>Email Adress</Text>
       <TextInput
         style={{
           color: colors.text,
           borderColor: colors.border,
+          borderRadius:10,
           ...styles.input,
         }}
         onChangeText={setParticipantName}
+        type='email'
         value={partipantName}
       />
 
-      <Text style={{color: colors.text}}>Room Name</Text>
+      <Text textAlign='left' style={{color: colors.text,alignItems:'flex-start',alignContent:'flex-start',textAlign:'left',justifyContent:'flex-start'}}>Password</Text>
       <TextInput
+       autoCapitalize={'none'}
+       autoCorrect={false}
+       secureTextEntry={true}
+       textContentType={'password'}
         style={{
           color: colors.text,
+          borderRadius:10,
           borderColor: colors.border,
           ...styles.input,
         }}
+        
         // onChangeText={setRoomName}
-        value={roomName}
+        // value={roomName}
       />
-      {isLoading ? (
+      <Text  textAlign='left' style={{color: colors.text,paddingBottom:20}}>Forgot Password</Text>
+      {/* {isLoading ? (
         <Button color={'red'} title="Connecting.." />
       ) : (
-        <Button color={'red'} title="Connect" onPress={()=> 
+        <Button containerViewStyle={{width: '100%', marginLeft: 0}} borderRadius={10} style={{width:'80%',borderRadius:20}} color={'red'} title="Sign In" onPress={()=> 
           navigation.navigate('OtpPage')
          
         }
           />
-      )}
+      )} */}
+        <Button buttonColor='#F47133' style={{width: '100%', marginLeft: 0,borderRadius:10}} mode="contained" onPress={() =>   navigation.navigate('OtpPage')}>
+        Sign In
+  </Button>
 
       <View style={styles.spacer} />
     </View>
@@ -63,8 +78,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    
     justifyContent: 'center',
-    paddingHorizontal: 10,
+    paddingHorizontal: 20,
+    paddingBottom:200
   },
   box: {
     width: 60,
